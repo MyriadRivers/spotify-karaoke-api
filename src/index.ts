9d6b886ffef6587f45da29b5dba495f84a04c034
Jason Gao <jgaominneapolis@gmail.com>
2023-11-26 16:34:09 -0500

    Enable websocket subscriptions

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,135 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { subscribe } from 'diagnostics_channel';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import cors from 'cors';
+import express from 'express';
+import { createServer } from 'http';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws'
+import { PubSub, withFilter } from 'graphql-subscriptions'
+
+const PORT = 4000;
+
+const pubsub = new PubSub();
 
 const typeDefs = `#graphql
     # Lyrics internally should always be a JSON string
     type Song {
-        name: String!
-        artists: [String!]!
-        duration: Float!
+        name: String
+        artists: [String!]
+        duration: Float
         id: String!
         lyrics: String
         url: String
     }
 
     type Query {
-        getSong(name: String!, artists: [String!]!, duration: Float!, id: String!): Song!
+        getSong(id: String!): Song!
     }
 
     type Mutation {
         requestKaraoke(name: String!, artists: [String!]!, duration: Float!, id: String!): Song!
-        addKaraoke(name: String!, artists: [String!]!, duration: Float!, id: String!, lyrics: String!, url: String!): Song!
+        addKaraoke(id: String!, lyrics: String!, url: String!): Song!
     }
 
     type Subscription {
         requestedKaraoke: Song!
         # @aws_subscribe(mutations: ["requestKaraoke"])
-        addedKaraoke(name: String!, artists: [String!]!, duration: Float!, id: String!): Song!
+        addedKaraoke(id: String!): Song!
         # @aws_subscribe(mutations: ["addKaraoke"])
     }
 `;
 
 const resolvers = {
     Query: {
-        getSong: (_, {name, artists, duration, id}: {name: string, artists: Array<string>, duration: number, id: string}) => {
+        getSong: (_, {id}: {id: string}) => {
             return {
-                name: name, 
-                artists: artists, 
-                duration: duration, 
                 id: id,
-            }
+            };
         },
     },
     Mutation: {
         requestKaraoke: (_, {name, artists, duration, id}: {name: string, artists: Array<string>, duration: number, id: string}) => {
+            pubsub.publish("KARAOKE_REQUESTED", {
+                requestedKaraoke: {
+                    name,
+                    artists,
+                    duration,
+                    id
+                }
+            });
             return {
                 name: name, 
                 artists: artists, 
                 duration: duration, 
                 id: id,
-            }
+            };
         },
-        addKaraoke: (_, {name, artists, duration, id, lyrics, url}: {name: string, artists: Array<string>, duration: number, id: string, lyrics: string, url: string}) => {
+        addKaraoke: (_, {id, lyrics, url}: {id: string, lyrics: string, url: string}) => {
+            pubsub.publish("KARAOKE_ADDED", {
+                addedKaraoke: {
+                    id,
+                    lyrics,
+                    url
+                }
+            })
             return {
-                name: name, 
-                artists: artists, 
-                duration: duration, 
                 id: id,
-            }
+                lyrics: lyrics,
+                url: url
+            };
         }
     },
     Subscription: {
-        requestedKaraoke: (_, {name, artists, duration, id}: {name: string, artists: Array<string>, duration: number, id: string}) => {
-            subscribe: 
+        requestedKaraoke: {
+            subscribe: () => pubsub.asyncIterator(["KARAOKE_REQUESTED"])
+        },
+        addedKaraoke: {
+            subscribe: withFilter(
+                () => pubsub.asyncIterator("KARAOKE_ADDED"),
+                (payload, variables) => {
+                    return (
+                        payload.addedKaraoke.id === variables.id
+                    )
+                }
+            )
         }
     }
 };
 
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const app = express();
+const httpServer = createServer(app);
+
+const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/graphql'
+});
+
+const serverCleanup = useServer({ schema }, wsServer);
+
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema,
+    plugins: [
+        ApolloServerPluginDrainHttpServer({ httpServer }),
+
+        {
+            async serverWillStart() {
+                return {
+                    async drainServer() {
+                        await serverCleanup.dispose();
+                    }
+                }
+            }
+        }
+    ]
 });
 
-const { url } = await startStandaloneServer(server, {
-    listen: {port: 4000},
-});
+await server.start();
+app.use("/graphql", cors<cors.CorsRequest>(), express.json(), expressMiddleware(server));
 
-console.log(`Server listening at: ${url}`)
+httpServer.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}/graphql`)
+});
